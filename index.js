12988bdabf2f67c231d4efcc55ac6a8c3762e313
Nouhal-sidani2000 <nuhalsidani@gmail.com>
2025-06-13 17:39:59 +0300

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ pool.on('error', (err) => {
 app.set('db', pool);
 
 // ✅ Middleware
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: 'https://iridescent-begonia-1b7fad.netlify.app', credentials: true }));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
